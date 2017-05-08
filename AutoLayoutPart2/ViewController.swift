2c4e8c2006f3f7d42fa39f4f91c0fbe3f98438fe
Owen Duignan <owenied@me.com>
2017-05-08 13:35:54 -0500

    Add Auto Layout anchors

--- AutoLayoutPart2/ViewController.swift
+++ AutoLayoutPart2/ViewController.swift
@@ -44,14 +44,32 @@ class ViewController: UIViewController {
         view.addSubview(label4)
         view.addSubview(label5)
         
-        let viewsDictionary = ["label1": label1,"label2": label2, "label3": label3, "label4": label4, "label5": label5]
-        let metrics = ["labelHeight": 88]
+//        let viewsDictionary = ["label1": label1,"label2": label2, "label3": label3, "label4": label4, "label5": label5]
+//        let metrics = ["labelHeight": 88]
+//        
+//        for label in viewsDictionary.keys {
+//            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
+//        }
+//        
+//        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]->=10-|", options: [], metrics: metrics, views: viewsDictionary))
         
-        for label in viewsDictionary.keys {
-            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
+        
+        // The following code adds anchors to the code and the labels are displayed in a similiar to the above code
+        var previous: UILabel!
+        
+        for label in [label1, label2, label3, label4, label5] {
+            label.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+            label.heightAnchor.constraint(equalToConstant: 88).isActive = true
+            
+            if previous != nil {
+                // we have a previous label - create a height constraint
+                label.topAnchor.constraint(equalTo: previous.bottomAnchor).isActive = true
+            }
+            
+            // Set the previous label to be the current one, for the next loop iteration
+            previous = label
         }
         
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]->=10-|", options: [], metrics: metrics, views: viewsDictionary))
     }
     
     override var prefersStatusBarHidden: Bool {
